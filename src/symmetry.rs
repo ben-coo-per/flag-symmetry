89c3f5624ab6d9a0001e7b258e90ea2b9dfdcebe
Ben Cooper <cooperben83@gmail.com>
2024-07-05 14:56:30 -0400

    added fuzzy matching for pixelated flags (circles don't do well)

--- src/symmetry.rs
+++ src/symmetry.rs
@@ -1,4 +1,5 @@
-use image::{DynamicImage, GenericImageView};
+use image::{DynamicImage, GenericImageView, Rgba};
+const COLOR_TOLERANCE: f32 = 0.25;
 
 #[derive(Debug)]
 pub struct Symmetry {
@@ -21,7 +22,7 @@ fn check_vertical_symmetry(img: &DynamicImage) -> bool {
         for x in 0..half_width {
             let left_pixel = img.get_pixel(x, y);
             let right_pixel = img.get_pixel(width - 1 - x, y);
-            if left_pixel != right_pixel {
+            if !compare_pixel(left_pixel, right_pixel) {
                 return false;
             }
         }
@@ -44,7 +45,7 @@ fn check_horizontal_symmetry(img: &DynamicImage) -> bool {
         for y in 0..half_height {
             let top_pixel = img.get_pixel(x, y);
             let bottom_pixel = img.get_pixel(x, height - 1 - y);
-            if top_pixel != bottom_pixel {
+            if !compare_pixel(top_pixel, bottom_pixel) {
                 return false;
             }
         }
@@ -59,4 +60,26 @@ pub fn check_symmetry(img: &DynamicImage) -> Symmetry {
         horizontal,
         vertical,
     }
+}
+
+fn compare_pixel(a: Rgba<u8>, b: Rgba<u8>) -> bool {
+    // Compare two pixels, returning true if they are within a certain tolerance of each other.
+    // Sum up the values of the RGB channels and compare them. If they are within the tolerance % of each other, return true.
+    let [r1, g1, b1, _a1] = a.0;
+    let [r2, g2, b2, _a2] = b.0;
+
+    let a_value = r1 as f32 + g1 as f32 + b1 as f32;
+    let b_value = r2 as f32 + g2 as f32 + b2 as f32;
+
+    // Calculate the average value to compare against
+    let average_value = (a_value + b_value) / 2.0;
+
+    // Avoid division by zero by checking if the average_value is zero
+    if average_value == 0.0 {
+        return a_value == b_value;
+    }
+
+    let diff = (a_value - b_value).abs();
+
+    (diff / average_value) < COLOR_TOLERANCE
 }
