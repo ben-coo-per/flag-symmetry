561de65d20d380ef22739d59ba59f1b76a1edd29
Ben Cooper <cooperben83@gmail.com>
2024-07-06 11:06:33 -0400

    improved presentation

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,11 @@ use fetch_flags::get_flag;
 use prettytable::{row, Table};
 use symmetry::check_symmetry;
 
-const CHUNK_SIZE: usize = 120;
+use indicatif::ProgressBar;
+
+use csv::Writer;
+
+const CHUNK_SIZE: usize = 126;
 const TIME_DELAY: u64 = 1;
 
 #[derive(Debug)]
@@ -24,11 +28,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut total_full = ReportingValue(0, String::new());
     let mut total_none = ReportingValue(0, String::new());
 
+    // Create a CSV file to store the results
+    let mut result_csv = Writer::from_path("results.csv")?;
+    result_csv.write_record(&["Flag", "Country Name", "Symmetry"])?;
+
     // Loop through all countries, but chunk into groups to avoid hitting rate limits
     let countries = consts::countries::COUNTRIES
         .iter()
         .map(|(code, (name, emoji))| (code.to_string(), (name.to_string(), emoji.to_string())))
         .collect::<Vec<(String, (String, String))>>();
+
+    // progress bar
+    println!("\n\nChecking flags for symmetry...");
+    let progress = ProgressBar::new(countries.iter().len() as u64);
 
     let chunked_countries = countries
         .chunks(CHUNK_SIZE)
@@ -42,27 +54,31 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let img: DynamicImage = get_flag(code).await?;
 
             let symmetry = check_symmetry(&img);
-            let sym_val;
+            let sym_val: &str;
 
             if symmetry.horizontal && symmetry.vertical {
                 update_table_total_value(&mut total_full, name);
-                sym_val = "🪩";
+                sym_val = "Full";
             } else if symmetry.vertical {
                 update_table_total_value(&mut total_vertical, name);
-                sym_val = "↔️";
+                sym_val = "Vert";
             } else if symmetry.horizontal {
                 update_table_total_value(&mut total_horizontal, name);
-                sym_val = "↕️";
+                sym_val = "Horz";
             } else {
                 update_table_total_value(&mut total_none, name);
-                sym_val = "❌";
+                sym_val = "None";
             }
 
-            println!("{} {}{}", sym_val, name.0, name.1);
+            // Write to CSV
+            result_csv.write_record(&[name.1.as_str(), name.0.as_str(), sym_val])?;
+            progress.inc(1);
         }
     }
 
-    report_results(total_full, total_horizontal, total_vertical, total_none).await;
+    result_csv.flush()?;
+    progress.finish_with_message("done");
+    report_result_summary(total_full, total_horizontal, total_vertical, total_none).await;
     Ok(())
 }
 
@@ -71,7 +87,7 @@ fn update_table_total_value(table_total: &mut ReportingValue, name: &(String, St
     table_total.1.push_str(name.1.as_str());
 }
 
-async fn report_results(
+async fn report_result_summary(
     total_full: ReportingValue,
     total_horizontal: ReportingValue,
     total_vertical: ReportingValue,
@@ -79,7 +95,8 @@ async fn report_results(
 ) {
     let mut table = Table::new();
 
-    println!("\nIn summary: ");
+    println!("\n\n\n");
+    println!("In summary: ");
     table.add_row(row!["🪩 Flags with full symmetry", total_full.0]);
     table.add_row(row!["↕️ Flags with horizontal symmetry", total_horizontal.0]);
     table.add_row(row!["↔️ Flags with vertical symmetry", total_vertical.0]);
@@ -88,9 +105,10 @@ async fn report_results(
     table.printstd();
 
     // delay for a sec
-    tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
-    println!("All full symmetry flags: {}", total_full.1);
-    println!("All horizontal symmetry flags: {}", total_horizontal.1);
-    println!("All vertical symmetry flags: {}", total_vertical.1);
-    println!("All no symmetry flags: {}", total_none.1);
+    tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+    println!("\n\n\n");
+    println!("Full symmetry: {}", total_full.1);
+    println!("Horizontal symmetry: {}", total_horizontal.1);
+    println!("Vertical symmetry: {}", total_vertical.1);
+    println!("No symmetry: {}", total_none.1);
 }
